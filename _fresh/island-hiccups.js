0d637ec4a6b5f62e175a1fd99922f0955d606edb
Airgeadlamh <eduaraguia@gmail.com>
2024-11-21 20:56:12 -0300

    Update

---
+++ _fresh/island-hiccups.js
@@ -0,0 +1,1 @@
+import{a as p}from"./chunk-EMOVIG5Q.js";import"./chunk-ULR4URCE.js";import"./chunk-LDV6YPMG.js";function u(){return p("p",{children:"4"})}export{u as default};
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, target-densitydpi=device-dpi, initial-scale=0, maximum-scale=1, user-scalable=yes" />
-        <title>Hiccup Counter</title>
+        <title>I Miss Yumemi</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body f-client-nav style="background-color:#d1aaf3; height:100%;">
