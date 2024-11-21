f86aa5f0d294fb22df04bcbf4e37c0b135088123
Airgeadlamh <eduaraguia@gmail.com>
2024-11-21 20:34:40 -0300

    Update

--- islands/IsLive.tsx
+++ islands/IsLive.tsx
@@ -1,68 +1,75 @@
 const YOUR_API_KEY = Deno.env.get("YTKEY");
+let live = ["", ""];
 export default function IsLive() {
-  let live = ["", ""];
-  live_info().then(res => {
-    live = res;
-  });
-
-  console.log(live[0]);
+  live_info();
+  //console.log(live)
   if (live[0] == "true") {
-  return (
-          <p>
-              <iframe width="420" height="315" src={"https://www.youtube.com/embed/" + live[1]}></iframe>
-          </p>
+    return (
+      <p>
+        <iframe width="420" height="315" src={"https://www.youtube.com/embed/" + live[1]}></iframe>
+      </p>
     );
   }
   else {
-  return (
-      <p style="color: cyan;">Currently dreaming</p>
-  );
+    return (
+        <p style="color: cyan;">Currently dreaming</p>
+    );
   }
 }
-let date = new Date();
-export async function live_info() {
+const date = new Date();
+
+export function live_info() {
   let earlyexit = false;
     let livestatus = "false";
-    //const channelid = "UCDqI2jOz0weumE8s7paEk6g"; // REPLACE WITH YOUR CHANNEL ID
+    //const channelid = "UCyKsg-57XC9pyHbP7v3kCPQ"; // REPLACE WITH YOUR CHANNEL ID
     const channelid = "UC2X643A7Hu0sqeet_wcJ-0g"; // REPLACE WITH YOUR CHANNEL ID
-    await Deno.readTextFile("lasthoursync").then(
-      function (response) {
-        const cur_hour = date.getHours();
-        const lastsync = parseInt(response);
-        if (cur_hour == lastsync) {
-          //console.log("Already checked this hour");
-          earlyexit = true;
-        }
-      }
-    )
+    const cur_hour = date.getHours();
+    const lastsync = Deno.env.get("lasthoursync");
+    //lastsync = "0";
+    let lasthour = 0;
+    if (lastsync != undefined) {
+      lasthour = parseInt(lastsync);
+    }
+    const laststatus = Deno.env.get("laststatus");
+    if (laststatus != undefined) {
+      livestatus = laststatus;
+    }
+    if (cur_hour == lasthour) {
+      earlyexit = true;
+    }
     if (earlyexit) {
-      const id = await Deno.readTextFile("lastid");
-      livestatus = await Deno.readTextFile("laststatus");
+      const id = Deno.env.get("lastid");
+      live = [livestatus, id];
       return [livestatus, id];
     }
-    await fetch('https://www.youtube.com/channel/' + channelid).then(function (response) {
+    fetch('https://www.youtube.com/channel/' + channelid).then(function (response) {
       return response.text();
     }).then(function (html) {
       if (html.includes("AO VIVO")) {
         livestatus = "true";
       }
+      else {
+        livestatus = "false";
+      }
+      Deno.env.set("laststatus", livestatus);
     }).catch(function (err) {
       console.log(err);
     });
     let id = "";
-    await fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' + channelid + '&order=date&eventType=completed&type=video&key=' + YOUR_API_KEY).then(function (response) {
+    fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=' + channelid + '&order=date&eventType=completed&type=video&key=' + YOUR_API_KEY).then(function (response) {
         return response.text();
       }).then(function (json) {
         const j = JSON.parse(json);
         console.log(j);
         id = j.items[0].id.videoId;
-        Deno.writeTextFile("lastid", id);
-        Deno.writeTextFile("laststatus", livestatus);
-        Deno.writeTextFile("lasthoursync", date.getHours().toString().trim());
+        Deno.env.set("lastdate", j.items[0].snippet.publishedAt);
+        Deno.env.set("lastid", id);
+        Deno.env.set("laststatus", livestatus);
+        Deno.env.set("lasthoursync", date.getHours().toString().trim());
       }).catch(function (err) {
-        livestatus = "Error"
+        livestatus = "Error";
+        Deno.env.set("laststatus", livestatus);
         //console.log(err);
       });
-
     return [livestatus, id];
 }
--- islands/LastLive.tsx
+++ islands/LastLive.tsx
@@ -2,9 +2,9 @@ import moment from "https://deno.land/x/momentjs@2.29.1-deno/mod.ts";
 
 export default function LastLive() {
     let firstDate = moment.utc().format("YYYY/MM/DD HH:mm:ss");
-    let secondDate = Deno.readTextFileSync("lastdate").replace("T", " ").replace("Z", "").replaceAll("-", "/");    
+    let secondDate = Deno.env.get("lastdate")?.toString().replace("T", " ").replace("Z", "").replaceAll("-", "/");
     let res = moment.utc(moment(firstDate,"YYYY/MM/DD HH:mm:ss").diff(moment(secondDate,"YYYY/MM/DD HH:mm:ss"))).format("HH:mm:ss");
-    let lastid = Deno.readTextFileSync("lastid");
+    let lastid = Deno.env.get("lastid");
     return (
         <div class="flex flex-col items-center">
             <p>Time since last stream</p>
