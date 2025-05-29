18b92c4beb307019875746c88545734e8b88f92d
MissingNoo <eduaraguia@gmail.com>
2025-05-29 18:09:24 -0300

    update

--- data.ts
+++ data.ts
@@ -1,11 +1,11 @@
 export const youtube_channel_id: string = "UC2X643A7Hu0sqeet_wcJ-0g";
 export const youtube_channel_link: string = "@yumemivt";
 export const twitch_channel_id: string = "yumemivt";
-export const base_url: string = "http://192.168.0.105:8000";
+export const base_url: string = "http://localhost:8000";
 //export const youtube_channel_id = "UC_WC6plpIdL_buebR9vSVkg";
 //export const twitch_channel_id = "alanzoka";
 import { MongoClient } from "npm:mongodb@6.1.0";
-const client = new MongoClient("mongodb://192.168.0.105:27017");
+const client = new MongoClient("mongodb://localhost:27017");
 //const client = new MongoClient("mongodb://0.tcp.sa.ngrok.io:15005");
 import { createClient } from "npm:redis@^4.5";
 export const redis = createClient({
