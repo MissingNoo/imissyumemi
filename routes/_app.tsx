16b11121682cb2f613cac5e7266cd7930cbeeabd
MissingNoo <eduaraguia@gmail.com>
2025-05-29 17:32:10 -0300

    update

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -4,6 +4,7 @@ export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
+      	<meta http-equiv="refresh" content="0; url=http://imissyumemi.337494.xyz/" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>imissyumemi</title>
