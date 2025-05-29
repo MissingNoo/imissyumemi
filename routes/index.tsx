b22771282ed19f5e62e8c00b1ee33043c09656b9
MissingNoo <eduaraguia@gmail.com>
2025-05-29 17:37:24 -0300

    update

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,14 +1,5 @@
 // deno-lint-ignore-file react-no-danger
-import { global_hiccups, yumemi_hiccups } from "./api/total_hiccups.ts";
 import { signal } from "@preact/signals";
-import { livestring } from "../routes/api/is_live.ts";
-import { time_since_last } from "./api/last_live.ts";
-import Hiccups from "../islands/Hiccups.tsx";
-const count = signal(global_hiccups);
-const ymmhic = signal(yumemi_hiccups);
-export function add_count() {
-  count.value++;
-}
 export function handler(req: Request): Response {
   return Response.redirect("https://imissyumemi.337494.xyz", 307);
 }
@@ -24,17 +15,6 @@ export default function Home() {
           height="128"
           alt="Yumemi Caelestis picture"
         />
-        <div
-          class="dreaming flex flex-col items-center justify-center"
-          dangerouslySetInnerHTML={{ __html: livestring }}
-        >
-        </div>
-        <div
-          class="dreaming flex flex-col items-center justify-center"
-          dangerouslySetInnerHTML={{ __html: time_since_last() }}
-        >
-        </div>
-        <Hiccups count={count} ymm={ymmhic} />
       </div>
     </div>
   );
